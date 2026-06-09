1cd9fcb1ec4710eb87ff60dfed1a3ba9ab9fc2e4
yuvrajjagtap48 <yuvrajjagtap2004@gmail.com>
2026-06-09 17:10:14 +0530

    09-06-2026

--- src/components/DigitalClock.jsx
+++ src/components/DigitalClock.jsx
@@ -1,21 +1,23 @@
-import { useState, useEffect } from 'react'
+import {useState, useEffect} from "react";
 
 const DigitalClock = () => {
-    const [time, setTime] = useState(new Date())
+  const [time, setTimer] = useState(new Date());
 
-    useEffect(() => {
-        const timer = setInterval(() => {
-            setTime(new Date())
-        }, 1000);
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimer(new Date());
+    }, 1000);
 
-        return () => clearInterval(timer)
-    }, [])
+    return () => 
+      clearInterval(timer);
+    }, []);
+
 
   return (
     <div>
-         <h1>Digital Clock</h1>
-      <div className="clock">
-        {time.toLocaleTimeString()}
+      <h1>Digital Clock</h1>
+      <div className="clock"> 
+        {time.toLocaleTimeString()};
       </div>
     </div>
   )
